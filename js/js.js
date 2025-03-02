9e6ab2092d0fcc988b415c095d788ce3015400af
glitchrgamer <glitchrgamer@gmail.com>
2025-03-02 14:03:25 +0200

    fixed

--- js/js.js
+++ js/js.js
@@ -22,14 +22,67 @@ ele.scrollIntoView({behavior:'smooth'})
 let openSider = document.getElementById('openSider')
 openSider.addEventListener('click',()=>{
 let sider =document.getElementById('sider')
-sider.classList.toggle('sider-show')
-sider.classList.toggle('sider-close')
+sider.classList.add('sider-show')
+sider.classList.remove('sider-close')
+let body = document.body 
+body.classList.add("overflow")
+document.querySelector(".overlay").classList.remove("display")
 }
 )
 let close = document.getElementById('close')
 close.addEventListener('click',()=>{
 let sider = document.getElementById('sider')
-sider.classList.toggle('sider-show')
-sider.classList.toggle('sider-close')
+sider.classList.remove('sider-show')
+sider.classList.add('sider-close')
+let body = document.body 
+body.classList.remove("overflow")
+document.querySelector(".overlay").classList.add("display")
 }
 )
+let social = document.querySelector('.instagram')
+
+window.addEventListener('scroll',()=>{
+    let rect = social.getBoundingClientRect();
+    if(rect.top < window.innerHeight - 200){
+        social.classList.add('show')
+        social.classList.remove('unshow')
+    }
+})
+let social2 = document.querySelector('.facebook')
+
+window.addEventListener('scroll',()=>{
+    let rect = social.getBoundingClientRect();
+    if(rect.top < window.innerHeight - 300){
+        social2.classList.add('show')
+        social2.classList.remove('unshow')
+    }
+})
+let social3 = document.querySelector('.tiktok')
+window.addEventListener('scroll',()=>{
+    let rect = social.getBoundingClientRect();
+    if(rect.top < window.innerHeight - 350){
+        social3.classList.add('show')
+        social3.classList.remove('unshow')
+    }
+})
+
+
+
+
+
+
+
+
+let headerDis = `<span>yassin nabil</span> ,MMA fighter amateur`;
+let matchDis=` Yassin Nabil is an amateur MMA fighter with a great passion for the sport. He has played three matches so far, winning two, losing one. Despite the challenges he's faced, Yassin continues to train hard and improve his skills, aiming for greater success in the future. He has a growing fanbase who supports him in every step, and he remains determined to achieve his goals in the world of MMA.`;
+let aboutYassin =`Yassin Nabil is an 18-year-old amateur MMA fighter, standing at 160 cm tall and competing in the weight range of 50 to 55 kg. He has been training in MMA for a year, gaining valuable experience in the sport. Yassin trains under the guidance of his coach, Omar El-Fayoumi, who has been instrumental in shaping his skills. With a strong dedication to improving his technique and expanding his knowledge of MMA`;
+let month =`--`;
+let day =`--`;
+let year =`----`;
+
+let nextMatchTime = `${day}/${month}/${year}`
+
+document.querySelector(".theWelcome").innerHTML = headerDis
+document.querySelector(".matchs-dis").innerHTML = matchDis
+document.querySelector(".about-dis").innerHTML = aboutYassin
+document.querySelector(".time").innerHTML = nextMatchTime
